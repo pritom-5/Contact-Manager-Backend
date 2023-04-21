29c43f52cd439911d04bcde0ba1bf30d4c62701f
pritom <pritom@pop-os.localdomain>
2023-04-21 13:42:05 +0600

    del: static files. Backend only serves as API build .4

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ app.use(
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "dist")));
-
 app.use(express.json());
 
 app.use("/api/users", require("./routes/userRoutes"));
